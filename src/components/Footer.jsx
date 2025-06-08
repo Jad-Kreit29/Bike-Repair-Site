3827161fecc49ab0cf1a603061eef91f1a64348b
Jad-Kreit29 <jkrei015@uottawa.ca>
2025-06-08 19:00:50 -0400

    Made a quick Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FaLinkedin } from 'react-icons/fa'
+
+const Footer = () => {
+  return (
+    <div>
+
+      <FaLinkedin />
+
+    </div>
+  )
+}
+
+export default Footer
