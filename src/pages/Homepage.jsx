617d132fa4a21173f1682ef6615bdbb4ade2cc18
Jad-Kreit29 <jkrei015@uottawa.ca>
2025-06-07 22:05:32 -0400

    Working on the HomePage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import RepairImage from '../images/repair_image.png'
 
 const Homepage = () => {
   return (
 
     <div className='bg-[#EAE0D5]'>
 
-      <h1 className='flex font-josefin text-6xl font-extrabold h-screen justify-center items-center'>MAIN PAGE!</h1>
+      <div className='flex justify-center items-center space-x-4 py-5'>
+
+        <img src={RepairImage} className='h-50 rounded-2xl'/>
+
+        <div className='ml-6'> 
+
+          <h1 className='flex font-josefin text-5xl font-extrabold'>Helping YOU move forward!</h1>
+
+        </div>
+
+
+      </div>
 
     </div>
 
