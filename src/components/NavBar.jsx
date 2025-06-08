616b1c4524a2ee3214dcb0bba066d729fb934ea9
Jad-Kreit29 <jkrei015@uottawa.ca>
2025-06-07 21:51:51 -0400

    Updated the NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import Logo from '../images/logo.svg'
 
 const NavBar = () => {
   return (
 
-    <nav className='bg-amber-400 font-mono px-4 py-2 border-b-4 border-[#01161E] sticky top-0 z-1'>
+    <nav className='bg-[#22333B] text-white text-lg font-mono px-4 py-2 border-b-4 border-[#01161E] sticky top-0 z-1'>
 
-        <div className='flex space-x-10'>
+      <div className='flex space-x-10 items-center-safe'>
 
-            <NavLink to='/'>Home</NavLink>
+          <NavLink to='/' className='flex space-x-2 items-center'>
 
-            <NavLink to='/contact'>Contact</NavLink>
+            <img src={Logo} className='w-25 h-25'/>
+            <p className='font-playfair text-4xl font-extrabold'>Bikety</p>
 
-            <NavLink to='/status'>Order Status</NavLink>
+          </NavLink>
 
-            <NavLink to='/appointment_book'>Book Appointment</NavLink>
+          <NavLink to='/contact'>Contact</NavLink>
+
+          <NavLink to='/status'>Order Status</NavLink>
+
+          <NavLink to='/appointment_book'>Book Appointment</NavLink>
 
 
-        </div>
+      </div>
 
     </nav>
 
