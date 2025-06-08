601d28a56833b46a40eee4c5ec915d165fda56a2
Jad-Kreit29 <jkrei015@uottawa.ca>
2025-06-08 19:59:42 -0400

    Added Contact component to Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,56 +1,63 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import RepairImage from '../images/repair_image.png'
+import Contact from '../components/Contact'
 
 const Homepage = () => {
   return (
 
-    <div className='bg-[#EFDECD]'>
+    <>
 
-      <div className='flex justify-center items-center space-x-4 py-15'>
+      <div className='bg-[#EFDECD]'>
 
-        {/* Image of Homepage */}
+        <div className='flex justify-center items-center space-x-4 py-15'>
 
-        <img className='object object-cover object-right h-100 w-120 rounded-xl' src={RepairImage}/>
+          {/* Image of Homepage */}
 
-        {/* Text of Homepage */}
+          <img className='object object-cover object-right h-100 w-120 rounded-xl' src={RepairImage}/>
 
-        <div className='ml-20 w-80'> 
+          {/* Text of Homepage */}
 
-          <h1 className='flex font-josefin text-4xl font-extrabold'>Helping YOU move forward!</h1>
+          <div className='ml-20 w-80'> 
 
-          <br />
+            <h1 className='flex font-josefin text-4xl font-extrabold'>Helping YOU move forward!</h1>
 
-          <p className='text-[18px] font-josefin'>
-            At bike mate, we make our mission to get you back up-to-speed to the things you love most and onto your travelling journey.
-            <br /><br />
-            With affordable repair pricing, and experts to help guide you and teach you, we have you covered every step of the way.
-          </p>
+            <br />
 
-          <br />
+            <p className='text-[18px] font-josefin'>
+              At bike mate, we make our mission to get you back up-to-speed to the things you love most and onto your travelling journey.
+              <br /><br />
+              With affordable repair pricing, and experts to help guide you and teach you, we have you covered every step of the way.
+            </p>
 
-          {/* Button */}
+            <br />
 
-          <div className='flex justify-center'>
+            {/* Button */}
 
-            <button className='bg-[#F71735] p-4 rounded-xl font-josefin font-extrabold text-white text-lg'>
+            <div className='flex justify-center'>
 
-              <Link to='/appointment_book'>
+              <button className='bg-[#F71735] p-4 rounded-xl font-josefin font-extrabold text-white text-lg'>
 
-                Book An Appointment!
-              
-              </Link>
+                <Link to='/appointment_book'>
 
-            </button>
+                  Book An Appointment!
+                
+                </Link>
+
+              </button>
+
+            </div>
+
 
           </div>
-
 
         </div>
 
       </div>
-
-    </div>
+      
+      <Contact />
+    
+    </>
 
   )
 }
