19c9cf8ac59d89b8b8c8d726880d552790f0955c
Jad-Kreit29 <jkrei015@uottawa.ca>
2025-06-08 18:56:29 -0400

    Finished Hero of Mainpage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,19 +1,50 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import RepairImage from '../images/repair_image.png'
 
 const Homepage = () => {
   return (
 
-    <div className='bg-[#EAE0D5]'>
+    <div className='bg-[#EFDECD]'>
 
-      <div className='flex justify-center items-center space-x-4 py-5'>
+      <div className='flex justify-center items-center space-x-4 py-15'>
 
-        <img src={RepairImage} className='h-60 rounded-2xl'/>
+        {/* Image of Homepage */}
 
-        <div className='ml-6'> 
+        <img className='object object-cover object-right h-100 w-120 rounded-xl' src={RepairImage}/>
 
-          <h1 className='flex font-josefin text-5xl font-extrabold'>Helping YOU move forward!</h1>
-          <p>Subtitle</p>
+        {/* Text of Homepage */}
+
+        <div className='ml-20 w-80'> 
+
+          <h1 className='flex font-josefin text-4xl font-extrabold'>Helping YOU move forward!</h1>
+
+          <br />
+
+          <p className='text-[18px] font-josefin'>
+            At bike mate, we make our mission to get you back up-to-speed to the things you love most and onto your travelling journey.
+            <br /><br />
+            With affordable repair pricing, and experts to help guide you and teach you, we have you covered every step of the way.
+          </p>
+
+          <br />
+
+          {/* Button */}
+
+          <div className='flex justify-center'>
+
+            <button className='bg-[#F71735] p-4 rounded-xl font-josefin font-extrabold text-white text-lg'>
+
+              <Link to='/appointment_book'>
+
+                Book An Appointment!
+              
+              </Link>
+
+            </button>
+
+          </div>
+
 
         </div>
 
