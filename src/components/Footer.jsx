8e9ac8bc17b7ea6a345da569e779012fab655ff8
MaximShainski <mxshain@gmail.com>
2025-06-08 19:53:16 -0400

    Changed colour scheme for appointment page and added footer logos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { FaLinkedin } from 'react-icons/fa'
+import { FaLinkedin, FaFacebook, FaInstagram }  from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>
-
+      <div className="flex justify-center items-center w-full py-4 gap-x-6 text-3xl">
       <FaLinkedin />
+      <FaFacebook />
+      <FaInstagram />
 
     </div>
   )
