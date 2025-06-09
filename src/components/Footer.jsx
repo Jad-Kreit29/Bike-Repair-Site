48dcb32ceb39e9a94ef4b118d51791d0efee61ff
Jad-Kreit29 <jkrei015@uottawa.ca>
2025-06-08 22:03:10 -0400

    Finished Order Status Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,18 @@ import { FaLinkedin, FaFacebook, FaInstagram }  from 'react-icons/fa'
 
 const Footer = () => {
   return (
-      <div className="flex justify-center items-center w-full py-4 gap-x-6 text-3xl">
-      <FaLinkedin />
-      <FaFacebook />
-      <FaInstagram />
 
-    </div>
+    <footer className='bottom-0'>
+
+      <div className="flex justify-center items-center py-4 gap-x-6 text-3xl">
+
+        <FaLinkedin />
+        <FaFacebook />
+        <FaInstagram />
+
+      </div>
+
+    </footer>
   )
 }
 
--- src/pages/OrderStatusPage.jsx
+++ src/pages/OrderStatusPage.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
 import { useState } from 'react'
+import ProgressBar from '../images/progress_bar.svg'
 
 
 const OrderStatusPage = () => {
 
   const [showStatus, setShowStatus] = useState(true);
   
-  let pageVisability = ""
+  let firstPageVisability = ""
+  let secondPageVisability = "hidden"
   
   if (!showStatus) {
   
-    pageVisability = "hidden"
+    firstPageVisability = "hidden"
+    secondPageVisability = ""
   
   }
 
   return (
-    <div className='bg-[#EFDECD]'>
+    <div className='bg-[#EFDECD] py-20'>
 
-      <div className='py-20'>
+      <h1 className='flex justify-center font-josefin font-extrabold text-5xl'>ORDER STATUS</h1>
+
+      {/* Input Order Status */}
+      <div className={firstPageVisability}>
             
-        {/* Main Page Text */}
-
-        <h1 className={`flex justify-center font-josefin font-extrabold text-5xl`}>ORDER STATUS</h1>
-
-        <p  className={`${pageVisability} flex mt-5 justify-center font-lato text-lg`}>Want to check the status of your order? Input your repair order number</p>
+        <p className='flex mt-5 justify-center font-lato text-lg'>Want to check the status of your order? Input your repair order number</p>
 
         <br />
 
@@ -31,7 +33,7 @@ const OrderStatusPage = () => {
 
         <div className='flex justify-center'>
 
-          <input type="number" name='ordernumber' placeholder="Repair Order Number:" className={`${pageVisability} bg-[#EFEFEF] w-100 h-10 p-5 font-josefin font-bold rounded-sm shadow-lg`}/>
+          <input type="text" name='ordernumber' placeholder="Repair Order Number:" className={`${firstPageVisability} bg-[#EFEFEF] w-100 h-10 p-5 font-josefin font-bold rounded-sm shadow-lg`}/>
 
         </div>
 
@@ -48,7 +50,29 @@ const OrderStatusPage = () => {
         </div>
 
       </div>      
-      
+
+      {/* Progress Bar Status */}
+      <div className={secondPageVisability}>
+
+        <div className='flex justify-center mt-10'>
+
+          <img src={ProgressBar} className='h-20'/>
+
+        </div>
+
+        <p className='flex justify-center mt-5 font-lato font-bold text-lg'>STAGE 2: REPAIR ANALYZED</p>
+
+        <p className='flex justify-center font-lato mt-2'>
+          We have determined the appropriate repair for your bike, and will begin the process soon!
+        </p>
+
+        <p className='flex justify-center font-lato'>
+          <br />
+          Your estimated repair date is: June 12th
+        </p>
+
+      </div>
+
     </div>
   )
 }
